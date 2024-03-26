5f58b226ec3c45a91e72a08922f77e6fe190ed73
mina041222 <minajeon2004@g.shingu.ac.kr>
2024-03-26 16:52:45 +0900

    마흔 한번째 자습

--- Hello Unity/Assets/Scripts/ThirdClassUnity/PlayerController.cs
+++ Hello Unity/Assets/Scripts/ThirdClassUnity/PlayerController.cs
@@ -4,40 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody;  //이동에 사용할 리지드바디
+    private Rigidbody playerRigidbody;  //이동에 사용할 리지드바디
     public float speed = 8.0f;          //이동 속력
 
     void Start()
     {
-        
+        //PlayRigidbody에 Rigidbody 할당
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, speed);
-        }
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
 
-        if(Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
-
-        if(Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
-
-        if(Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
+        //실제 이동 속도를 입력값과 이동 속력을 사용해 결정
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+        //Vector3 속도를 (xSpeed,0,zSpeed) 로 생성
+        Vector3 newVelocity = new Vector3(xSpeed, 0, zSpeed);
+        playerRigidbody.velocity = newVelocity;
     }
 
     public void Die()
     {
+        //게임 오브젝트 비활성화
         gameObject.SetActive(false);
     }
 }
