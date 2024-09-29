1e8902fb33431824db45a10e2f6472ee0845f8ce
mina041222 <minajeon2004@g.shingu.ac.kr>
2024-09-29 22:42:24 +0900

    [feat]공부 2일차

--- My project/Assets/Scripts/Hello Unity.cs
+++ My project/Assets/Scripts/Hello Unity.cs
@@ -7,19 +7,13 @@ public class HelloUnity : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //케릭터 프로필을 변수로 만들기
-        string characerName = "라라";
-        char bloodType = 'A';
-        int age = 17;
-        float height = 173.8f;
-        bool isFemale = true;
+        int i = 0;
 
-        //생성한 변수를 콘솔에 출력
-        Debug.Log("케릭터 이름 : " + characerName);
-        Debug.Log("혈액형:" + bloodType);
-        Debug.Log("나이:" + age);
-        Debug.Log("키:" + height);
-        Debug.Log("여성인가: " + isFemale);
+        while (i < 10)
+        {
+            Debug.Log(i + "번째 루트입니다");
+            i++
+        }
     }
 
     // Update is called once per frame
