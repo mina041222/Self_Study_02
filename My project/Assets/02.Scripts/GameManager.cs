4883952ee2d7039f3e205015df2a2bdfa9301294
mina041222 <minajeon2004@g.shingu.ac.kr>
2024-10-07 10:58:20 +0900

    [feat]PlayerPrefs 배우기

--- My project/Assets/02.Scripts/GameManager.cs
+++ My project/Assets/02.Scripts/GameManager.cs
@@ -4,17 +4,58 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+
 public class GameManager : MonoBehaviour
 {
+    public GameObject gameOverText;
+    public Text timeText;
+    public Text recordText;
+
+    private float surviveTime;
+    private bool isGameOver;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        surviveTime = 0;
+        isGameOver = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!isGameOver)
+        {
+            surviveTime += Time.deltaTime;
+            timeText.text = "Time: " + (int)surviveTime;
+
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    public void EndGame()
+    {
+        isGameOver = true;
+        gameOverText.SetActive(true);
+
+        float bestTime = PlayerPrefs.GetFloat("BestTime");
+
+        if(surviveTime > bestTime)
+        {
+            bestTime = surviveTime;
+            PlayerPrefs.SetFloat("BestTime", bestTime);
+
+
+        }
+
+        recordText.text = "Best Time: " + (int)bestTime;
+
     }
 }
